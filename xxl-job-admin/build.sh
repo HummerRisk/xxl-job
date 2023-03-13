645a69d2eaa2de30e8ae2c907da6b3af7df0d05f
guohao <402847591@qq.com>
2023-03-14 04:07:16 +0800

    feat(hummer): 添加hummer配置

--- xxl-job-admin/build.sh
+++ xxl-job-admin/build.sh
@@ -2,6 +2,6 @@
 echo "构建镜像 ..."
 mvn clean package -X -U -Dmaven.test.skip=true
 
-docker build -t registry.cn-beijing.aliyuncs.com/hummerrisk/hummer-job:v1.0 .
-docker push registry.cn-beijing.aliyuncs.com/hummerrisk/hummer-job:v1.0
+docker build -t registry.cn-beijing.aliyuncs.com/hummerrisk/hmr-job:v1.0 .
+docker push registry.cn-beijing.aliyuncs.com/hummerrisk/hmr-job:v1.0
 docker images|grep hummer-job|awk '{print "docker rmi -f "$3}'|sh
